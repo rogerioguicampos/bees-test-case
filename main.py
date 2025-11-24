1140adf72d9078d8564746492bd0b2ab53de480b
rogerioguicampos <rogerio.guicampos@gmail.com>
2025-11-24 20:54:18 -0300

    Add docstring delete parquet func + log

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 from datetime import datetime
+import logging
 import pandas as pd
 from pathlib import Path
 import requests
 import shutil
+import time
 
 
-string_date = datetime.now().strftime('%Y_%m_%d')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+STRING_DATE = datetime.now().strftime('%Y_%m_%d')
 
-def delete_partition_recursively(partition_path: str='data/'):
+def delete_partition_recursively(partition_path: str = 'data/') -> None:
+    """
+    Recursively deletes a directory if it exists and contains .parquet files.
+ 
+    Args:
+        partition_path (str): The path to the directory to be cleaned.
+    """
     target_dir = Path(partition_path)
 
     if not target_dir.exists():
-        print(f"Directory not found: {partition_path}")
+        logging.info(f"Directory not found: {partition_path}")
         return
 
     has_parquet = any(target_dir.rglob("*.parquet"))
 
     if not has_parquet:
-        print(f"No .parquet files found in {target_dir.name}. Skipping deletion.")
+        logging.warning(f"No .parquet files found in {target_dir.name}. Skipping deletion.")
         return
 
     try:
         shutil.rmtree(target_dir)
-        print(f"Successfully deleted partition and all subfolders: {target_dir.name}")
+        logging.info(f"Successfully deleted partition and all subfolders: {target_dir.name}")
     except OSError as e:
-        print(f"Error removing directory {target_dir.name}: {e}")
+        logging.error(f"Error removing directory {target_dir.name}: {e}")
 
 
 def bronze_layer(url: str='https://api.openbrewerydb.org/v1/breweries'):
     raw_json_data = requests.get(url).json()
     df = pd.DataFrame(raw_json_data)
-    df['date_request'] = string_date
+    df['date_request'] = STRING_DATE
 
-    delete_partition_recursively(partition_path=f'data/bronze/date_request={string_date}')
+    delete_partition_recursively(partition_path=f'data/bronze/date_request={STRING_DATE}')
     df.to_parquet(path=f'data/bronze/', partition_cols=['date_request'])
 
 
 def silver_layer():
     df = pd.read_parquet('data/bronze/')
 
-    delete_partition_recursively(partition_path=f'data/silver/date_request={string_date}')
+    delete_partition_recursively(partition_path=f'data/silver/date_request={STRING_DATE}')
     df.to_parquet(path='data/silver/', partition_cols=['date_request', 'country'])
 
 
@@ -54,9 +63,9 @@ def gold_layer(list_agg: list=['brewery_type', 'country', 'state_province']):
         .rename(columns={0: 'count'})
     )
 
-    df_agg['date_request'] = string_date
+    df_agg['date_request'] = STRING_DATE
 
-    delete_partition_recursively(partition_path=f'data/gold/date_request={string_date}')
+    delete_partition_recursively(partition_path=f'data/gold/date_request={STRING_DATE}')
     df_agg.to_parquet(path='data/gold/', partition_cols=['date_request'])
 
 
